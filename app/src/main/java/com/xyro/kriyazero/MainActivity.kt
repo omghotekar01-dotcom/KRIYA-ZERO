e392242944df76d4d649045a9841ba8aeeef1f1c
Omiiii <omghotekar01@gmail.com>
2026-09-01 22:13:31 +0530

    feat: add Compose app entry point

---
+++ app/src/main/java/com/xyro/kriyazero/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.xyro.kriyazero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.xyro.kriyazero.ui.KriyaApp
+import com.xyro.kriyazero.ui.theme.KriyaZeroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KriyaZeroTheme {
+                KriyaApp()
+            }
+        }
+    }
+}
